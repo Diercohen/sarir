bee1cc667d3a40bfd7ecd6f65b31c58b82790b06
Dier Cohen <i.am.dier.cohen@gmail.com>
2025-10-25 00:42:16 +0330

    feat: enhance Topbar component by adding a Settings button and restructuring layout for better usability

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -7,24 +7,34 @@ import {
   MenubarShortcut,
   MenubarTrigger,
 } from "@/components/ui/menubar";
+import { SettingsIcon } from "lucide-react";
 import type { FC } from "react";
+import { Button } from "../ui/button";
 
 const Topbar: FC = () => {
   return (
     <Menubar className="border-0 shadow-none mx-2 fixed top-0 left-0 right-0 z-50">
-      <MenubarMenu>
-        <MenubarTrigger>File</MenubarTrigger>
-        <MenubarContent>
-          <MenubarItem>
-            New Tab <MenubarShortcut>⌘T</MenubarShortcut>
-          </MenubarItem>
-          <MenubarItem>New Window</MenubarItem>
-          <MenubarSeparator />
-          <MenubarItem>Share</MenubarItem>
-          <MenubarSeparator />
-          <MenubarItem>Print</MenubarItem>
-        </MenubarContent>
-      </MenubarMenu>
+      <div className=" w-full flex justify-between items-center">
+        <div>
+          <MenubarMenu>
+            <MenubarTrigger>File</MenubarTrigger>
+            <MenubarContent>
+              <MenubarItem>
+                New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+              </MenubarItem>
+              <MenubarItem>New Window</MenubarItem>
+              <MenubarSeparator />
+              <MenubarItem>Share</MenubarItem>
+              <MenubarSeparator />
+              <MenubarItem>Print</MenubarItem>
+            </MenubarContent>
+          </MenubarMenu>
+        </div>
+        <Button variant="ghost" size="sm" aria-label="Submit">
+          <SettingsIcon className="size-4" />
+          <span className="text-sm">Settings</span>
+        </Button>
+      </div>
     </Menubar>
   );
 };
