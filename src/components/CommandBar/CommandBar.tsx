a397f44410ab97fac7fab81ada2f4d8626ea327f
Dier Cohen <i.am.dier.cohen@gmail.com>
2025-10-25 00:35:59 +0330

    refactor: replace Menubar with a simplified CommandBar layout using a Button component for improved user interaction

--- src/components/CommandBar/CommandBar.tsx
+++ src/components/CommandBar/CommandBar.tsx
@@ -1,31 +1,14 @@
-import {
-  Menubar,
-  MenubarContent,
-  MenubarItem,
-  MenubarMenu,
-  MenubarSeparator,
-  MenubarShortcut,
-  MenubarTrigger,
-} from "@/components/ui/menubar";
+import { ArrowUpIcon } from "lucide-react";
 import type { FC } from "react";
+import { Button } from "../ui/button";
 
 const CommandBar: FC = () => {
   return (
-    <Menubar className="fixed top-10 left-0 right-0 z-50">
-      <MenubarMenu>
-        <MenubarTrigger>File</MenubarTrigger>
-        <MenubarContent>
-          <MenubarItem>
-            New Tab <MenubarShortcut>⌘T</MenubarShortcut>
-          </MenubarItem>
-          <MenubarItem>New Window</MenubarItem>
-          <MenubarSeparator />
-          <MenubarItem>Share</MenubarItem>
-          <MenubarSeparator />
-          <MenubarItem>Print</MenubarItem>
-        </MenubarContent>
-      </MenubarMenu>
-    </Menubar>
+    <div className="px-2 fixed top-10 left-0 right-0 z-50">
+      <Button variant="outline" size="icon" aria-label="Submit">
+        <ArrowUpIcon />
+      </Button>
+    </div>
   );
 };
 
