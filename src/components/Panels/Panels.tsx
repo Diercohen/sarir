cd252f591b19b07af6feb1544955821df43e38fb
Dier Cohen <i.am.dier.cohen@gmail.com>
2025-10-27 16:41:30 +0330

    feat(panel): add some sample panel

--- src/components/Panels/Panels.tsx
+++ src/components/Panels/Panels.tsx
@@ -1,9 +1,22 @@
+import { LayoutDashboardIcon } from "lucide-react";
 import type { FC } from "react";
+import { Button } from "../ui/button";
 
 const Panels: FC = () => {
   return (
-    <div>
-      <h1>Panels</h1>
+    <div className="fixed top-20 left-0 w-[100px] bottom-0 bg-gray-700 flex flex-col">
+      <div className="flex-1">
+        <div className="h-full w-full p-2">
+          <Button
+            variant="secondary"
+            color="white"
+            size="sm"
+            aria-label="Submit"
+          >
+            <LayoutDashboardIcon className="size-4" />
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
