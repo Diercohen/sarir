f3c576843c5f175dd9b807d5bba1d7357b544ef4
Dier Cohen <i.am.dier.cohen@gmail.com>
2025-10-28 17:00:33 +0330

    style: update Panels component layout by adjusting top margin and changing button variant for improved usability

--- src/components/Panels/Panels.tsx
+++ src/components/Panels/Panels.tsx
@@ -4,12 +4,12 @@ import { Button } from "../ui/button";
 
 const Panels: FC = () => {
   return (
-    <div className="fixed top-18 left-0  bottom-0 bg-neutral-700 flex flex-col">
+    <div className="fixed top-22 left-0  bottom-0 bg-neutral-700 flex flex-col">
       <div className="flex-1 border-r border-neutral-600">
         <div className="h-full w-full ">
           <div className="grid grid-cols-1 gap-4">
             <Button
-              variant="ghost"
+              variant="default"
               size="lg"
               className="w-full flex items-center gap-2 justify-start border-y rounded-none border-neutral-500"
             >
