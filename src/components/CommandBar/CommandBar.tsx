3e7d235ef21f0b5e12402af88454bede9207b147
Dier Cohen <i.am.dier.cohen@gmail.com>
2025-10-28 16:59:43 +0330

    style: enhance CommandBar by adding Redo button and grouping buttons for improved navigation

--- src/components/CommandBar/CommandBar.tsx
+++ src/components/CommandBar/CommandBar.tsx
@@ -1,13 +1,19 @@
-import { Undo2 } from "lucide-react";
+import { Redo2, Undo2 } from "lucide-react";
 import type { FC } from "react";
 import { Button } from "../ui/button";
+import { ButtonGroup } from "../ui/button-group";
 
 const CommandBar: FC = () => {
   return (
     <div className="px-2 fixed top-9 left-0 right-0 z-50 bg-neutral-700 py-2">
-      <Button variant="default" size="icon" aria-label="Submit">
-        <Undo2 />
-      </Button>
+      <ButtonGroup className="hidden sm:flex">
+        <Button variant="default" size="icon" aria-label="Go Back">
+          <Undo2 />
+        </Button>
+        <Button variant="default" size="icon" aria-label="Go Forward">
+          <Redo2 />
+        </Button>
+      </ButtonGroup>
     </div>
   );
 };
