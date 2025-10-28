8c060926d3b75fd31dacb1ad9fd8f2a8454523af
Dier Cohen <i.am.dier.cohen@gmail.com>
2025-10-28 16:57:50 +0330

    style: replace ArrowUpIcon with Undo2 in CommandBar and update button variant for improved functionality

--- src/components/CommandBar/CommandBar.tsx
+++ src/components/CommandBar/CommandBar.tsx
@@ -1,12 +1,12 @@
-import { ArrowUpIcon } from "lucide-react";
+import { Undo2 } from "lucide-react";
 import type { FC } from "react";
 import { Button } from "../ui/button";
 
 const CommandBar: FC = () => {
   return (
-    <div className="px-2 fixed top-9 left-0 right-0 z-50 bg-neutral-700">
-      <Button variant="outline" size="icon" aria-label="Submit">
-        <ArrowUpIcon />
+    <div className="px-2 fixed top-9 left-0 right-0 z-50 bg-neutral-700 py-2">
+      <Button variant="default" size="icon" aria-label="Submit">
+        <Undo2 />
       </Button>
     </div>
   );
