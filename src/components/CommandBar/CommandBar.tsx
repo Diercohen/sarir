2f7dd5f35967ab8f27798a060be4e39f4b16e247
Dier Cohen <i.am.dier.cohen@gmail.com>
2025-10-28 16:45:54 +0330

    style: update CommandBar background color to neutral-700 for improved visual consistency

--- src/components/CommandBar/CommandBar.tsx
+++ src/components/CommandBar/CommandBar.tsx
@@ -4,7 +4,7 @@ import { Button } from "../ui/button";
 
 const CommandBar: FC = () => {
   return (
-    <div className="px-2 fixed top-10 left-0 right-0 z-50">
+    <div className="px-2 fixed top-10 left-0 right-0 z-50 bg-neutral-700">
       <Button variant="outline" size="icon" aria-label="Submit">
         <ArrowUpIcon />
       </Button>
