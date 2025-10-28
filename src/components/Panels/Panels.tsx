26e022fcd8cb554db53d076a899aaf6598b09483
Dier Cohen <i.am.dier.cohen@gmail.com>
2025-10-28 16:56:09 +0330

    style: enhance Panels component layout by adjusting margins, adding borders, and updating button styles for improved aesthetics and usability

--- src/components/Panels/Panels.tsx
+++ src/components/Panels/Panels.tsx
@@ -4,17 +4,18 @@ import { Button } from "../ui/button";
 
 const Panels: FC = () => {
   return (
-    <div className="fixed top-20 left-0 w-[100px] bottom-0 bg-neutral-700 flex flex-col">
-      <div className="flex-1">
-        <div className="h-full w-full p-2">
-          <Button
-            variant="secondary"
-            color="white"
-            size="sm"
-            aria-label="Submit"
-          >
-            <LayoutDashboardIcon className="size-4" />
-          </Button>
+    <div className="fixed top-18 left-0  bottom-0 bg-neutral-700 flex flex-col">
+      <div className="flex-1 border-r border-neutral-600">
+        <div className="h-full w-full ">
+          <div className="grid grid-cols-1 gap-4">
+            <Button
+              variant="ghost"
+              size="lg"
+              className="w-full flex items-center gap-2 justify-start border-y rounded-none border-neutral-500"
+            >
+              <LayoutDashboardIcon className="size-4" />
+            </Button>
+          </div>
         </div>
       </div>
     </div>
