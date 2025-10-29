1a20546bd461049aebebd1293585d61e4233b805
Dier Cohen <i.am.dier.cohen@gmail.com>
2025-10-29 07:22:58 +0330

    style: update Canvas component layout with fixed positioning and padding for improved responsiveness

--- src/components/Canvas/Canvas.tsx
+++ src/components/Canvas/Canvas.tsx
@@ -2,7 +2,7 @@ import type { FC } from "react";
 
 const Canvas: FC = () => {
   return (
-    <div>
+    <div className="fixed top-20 left-12 p-1 w-[calc(100%-3rem)] h-full">
       <h1>Canvas</h1>
     </div>
   );
