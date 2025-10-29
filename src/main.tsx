78cf30302369a37989d024cde5331a144549326a
Dier Cohen <i.am.dier.cohen@gmail.com>
2025-10-29 07:12:00 +0330

    feat: wrap App component with ThemeProvider for theme management

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import { AppContextProvider } from "./App.context.tsx";
 import App from "./App.tsx";
 import "./index.css";
+import { ThemeProvider } from "./components/theme-provider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AppContextProvider>
-      <App />
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <App />
+      </ThemeProvider>
     </AppContextProvider>
   </StrictMode>
 );
