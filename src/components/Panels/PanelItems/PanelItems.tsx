473e551812594d04efd9b22dc76d8cf9e924607e
Dier Cohen <i.am.dier.cohen@gmail.com>
2025-11-14 19:45:05 +0330

    feat: integrate TextTool component into PanelItems for enhanced text editing interface

--- src/components/Panels/PanelItems/PanelItems.tsx
+++ src/components/Panels/PanelItems/PanelItems.tsx
@@ -1,7 +1,12 @@
 import type { FC } from "react";
+import TextTool from "./TextTool";
 
 const PanelItems: FC = () => {
-  return <></>;
+  return (
+    <>
+      <TextTool />
+    </>
+  );
 };
 
 export default PanelItems;
