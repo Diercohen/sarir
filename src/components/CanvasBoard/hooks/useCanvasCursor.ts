fa8171b1b6f22ebab8983aaa7bd04f5dfd367ccf
Dier Cohen <i.am.dier.cohen@gmail.com>
2025-11-14 20:33:07 +0330

    feat: enhance useCanvasCursor hook to apply dynamic cursor styles to canvas elements based on active tool

--- src/components/CanvasBoard/hooks/useCanvasCursor.ts
+++ src/components/CanvasBoard/hooks/useCanvasCursor.ts
@@ -1,5 +1,8 @@
 import { ToolsIcon, ToolType } from "@/App.const";
 import { useAppContext } from "@/App.context";
+import * as fabric from "fabric";
+import { useEffect } from "react";
+import { getCanvas } from "../code";
 
 const useCanvasCursor = (isDragging: boolean) => {
   const { activeTool } = useAppContext();
@@ -11,6 +14,31 @@ const useCanvasCursor = (isDragging: boolean) => {
         ToolsIcon[activeTool].icon
       )}") 12 12, auto`
     : "grab";
+
+  useEffect(() => {
+    let canvasInstance: fabric.Canvas | null = null;
+
+    try {
+      canvasInstance = getCanvas() as fabric.Canvas;
+    } catch {
+      return;
+    }
+
+    const applyCursor = (element?: HTMLElement | HTMLCanvasElement | null) => {
+      if (element) {
+        element.style.cursor = canvasCursor;
+      }
+    };
+
+    canvasInstance.defaultCursor = canvasCursor;
+    canvasInstance.hoverCursor = canvasCursor;
+    canvasInstance.moveCursor = canvasCursor;
+    canvasInstance.freeDrawingCursor = canvasCursor;
+
+    applyCursor(canvasInstance.upperCanvasEl);
+    applyCursor(canvasInstance.lowerCanvasEl);
+    applyCursor(canvasInstance.wrapperEl);
+  }, [canvasCursor]);
 
   return { canvasCursor };
 };
