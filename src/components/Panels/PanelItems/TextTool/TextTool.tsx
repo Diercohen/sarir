4ff08c742019cd35e0e391e20821b4bc5fad7306
Dier Cohen <i.am.dier.cohen@gmail.com>
2025-11-14 19:50:58 +0330

    fix: update button variant in TextTool component to reflect active tool state for better user feedback

--- src/components/Panels/PanelItems/TextTool/TextTool.tsx
+++ src/components/Panels/PanelItems/TextTool/TextTool.tsx
@@ -5,14 +5,14 @@ import { TypeIcon } from "lucide-react";
 import type { FC } from "react";
 
 const TextTool: FC = () => {
-  const { setActiveTool } = useAppContext();
+  const { setActiveTool, activeTool } = useAppContext();
   return (
     <>
       <Button
         onClick={() => {
           setActiveTool(ToolType.TextTool);
         }}
-        variant="outline"
+        variant={activeTool === ToolType.TextTool ? "default" : "outline"}
         size="lg"
         className="w-full flex items-center gap-2 justify-start border-y rounded-none dark:border-neutral-500 border-neutral-200"
       >
