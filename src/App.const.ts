744fe359088553649e374dd7c84dd255fae4ca18
Dier Cohen <i.am.dier.cohen@gmail.com>
2025-11-14 20:18:59 +0330

    feat: add ToolsIcon constant to map ToolType to rendered icons for improved tool representation

--- src/App.const.ts
+++ src/App.const.ts
@@ -1,3 +1,7 @@
+import { TypeIcon } from "lucide-react";
+import { createElement } from "react";
+import { renderToStaticMarkup } from "react-dom/server";
+
 export enum Language {
   EN = "en",
   FA = "fa",
@@ -6,3 +10,9 @@ export enum Language {
 export enum ToolType {
   TextTool = "TEXT_TOOL",
 }
+
+export const ToolsIcon: Record<ToolType, { icon: string }> = {
+  [ToolType.TextTool]: {
+    icon: renderToStaticMarkup(createElement(TypeIcon)),
+  },
+};
