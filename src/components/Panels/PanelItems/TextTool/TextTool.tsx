7d570a03c83513189d69d62411f840f793f44a3f
Dier Cohen <i.am.dier.cohen@gmail.com>
2025-11-14 19:49:12 +0330

    feat: implement active tool selection in TextTool component for improved user interaction

--- src/components/Panels/PanelItems/TextTool/TextTool.tsx
+++ src/components/Panels/PanelItems/TextTool/TextTool.tsx
@@ -1,11 +1,17 @@
+import { ToolType } from "@/App.const";
+import { useAppContext } from "@/App.context";
 import { Button } from "@/components/ui/button";
 import { TypeIcon } from "lucide-react";
 import type { FC } from "react";
 
 const TextTool: FC = () => {
+  const { setActiveTool } = useAppContext();
   return (
     <>
       <Button
+        onClick={() => {
+          setActiveTool(ToolType.TextTool);
+        }}
         variant="outline"
         size="lg"
         className="w-full flex items-center gap-2 justify-start border-y rounded-none dark:border-neutral-500 border-neutral-200"
