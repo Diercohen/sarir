7aa4a80f8c51ca2c0efcfe782542adb2136216d4
Dier Cohen <i.am.dier.cohen@gmail.com>
2025-11-14 20:10:47 +0330

    feat: add ToolItem component for tool selection in Panels, enhancing user interaction

--- src/components/Panels/PanelItems/ToolItem/ToolItem.tsx
+++ src/components/Panels/PanelItems/ToolItem/ToolItem.tsx
@@ -1,27 +1,28 @@
-import { ToolType } from "@/App.const";
+import { ToolsIcon, ToolType } from "@/App.const";
 import { useAppContext } from "@/App.context";
 import { Button } from "@/components/ui/button";
-import { TypeIcon } from "lucide-react";
 import type { FC } from "react";
 
-const TextTool: FC = () => {
+const ToolItem: FC<{ toolType: ToolType }> = ({ toolType }) => {
   const { setActiveTool, activeTool } = useAppContext();
   return (
     <>
       <Button
         onClick={() => {
-          setActiveTool((prev) =>
-            prev === ToolType.TextTool ? undefined : ToolType.TextTool
-          );
+          setActiveTool((prev) => (prev === toolType ? undefined : toolType));
         }}
-        variant={activeTool === ToolType.TextTool ? "default" : "outline"}
+        variant={activeTool === toolType ? "default" : "outline"}
         size="lg"
         className="w-full flex items-center gap-2 justify-start border-y rounded-none dark:border-neutral-500 border-neutral-200"
       >
-        <TypeIcon className="size-4" />
+        <span
+          dangerouslySetInnerHTML={{
+            __html: ToolsIcon[toolType].icon,
+          }}
+        />
       </Button>
     </>
   );
 };
 
-export default TextTool;
+export default ToolItem;
