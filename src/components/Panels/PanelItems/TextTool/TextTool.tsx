bfcc9c2739c2f83dad8e9d9b0f769b79b4067d9c
Dier Cohen <i.am.dier.cohen@gmail.com>
2025-11-14 19:44:55 +0330

    feat: add TextTool component for enhanced text editing functionality in Panels

---
+++ src/components/Panels/PanelItems/TextTool/TextTool.tsx
@@ -0,0 +1,19 @@
+import { Button } from "@/components/ui/button";
+import { TextIcon } from "lucide-react";
+import type { FC } from "react";
+
+const TextTool: FC = () => {
+  return (
+    <>
+      <Button
+        variant="outline"
+        size="lg"
+        className="w-full flex items-center gap-2 justify-start border-y rounded-none dark:border-neutral-500 border-neutral-200"
+      >
+        <TextIcon className="size-4" />
+      </Button>
+    </>
+  );
+};
+
+export default TextTool;
